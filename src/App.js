dc69de6c70b6b19182d5c984ce1f9b90540c1da3
Mahmoud Abdelhameed <mahardan@hotmail.com>
2023-11-06 23:23:33 +0200

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,42 +1,63 @@
-import './App.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
-  //console.log('we are about to list the employees');
-  const [role, setRole] = useState('dev');
-  //let role = 'dev';
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: "https://th.bing.com/th?id=ORMS.bc97f2501e728bea41772c453e09396e&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1&p=0",
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/6194365/pexels-photo-6194365.jpeg",
+    },
+    {
+      name: "John",
+      role: "Director of Eng.",
+      img: "https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Melanie",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Corey",
+      role: "Devops Engineer",
+      img: "https://images.pexels.com/photos/4890259/pexels-photo-4890259.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Jake",
+      role: "Senior Intern",
+      img: "https://images.pexels.com/photos/5384429/pexels-photo-5384429.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ]);
   const showEmplyees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className='App'>
-        {/*console.log('inside the return')*/}
         {showEmplyees ? (
           <>
-               <input type='text' onChange={(e) => {
-                console.log(e.target.value);
-                //role = e.target.value;
-                setRole(e.target.value);
-               }} />
-               <Employee name="Caleb" role="Intern"/>
-               <Employee name="Abby" role={role} />
-               <Employee name="John" />
+            <div className="flex flex-wrap justify-center">
+              {employees.map((employee) => {
+                return(
+                  <Employee
+                    key={uuidv4()} 
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                  />
+                );
+              })}
+            </div>
           </>
         ) : (
-            <p>You cannot see the employees</p>
+          <p>You cannot see the employees</p>
         )}
-      </div>
     </div>
   );
 }
